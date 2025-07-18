c9535ec5c35024763e25942014f14dc33edc4bbb
txrcia <tercia221@gmail.com>
2025-07-18 11:35:29 +0400

    Update: all files with latest changes

--- Anomaly_page.py
+++ Anomaly_page.py
@@ -7,8 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 import plotly.express as px
 
 def anomaly_detection():
-    st.set_page_config(page_title="Passenger Anomaly Detection", layout="wide")
-
+    
     st.title("🛑 Airline Passenger Anomaly Detection Dashboard")
     st.markdown("""
     Detect unusual passenger behavior or feedback patterns based on service ratings, delays, and loyalty status.
